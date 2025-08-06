d49e23a1146ecce96bc8acc7d756a65dd198cf29
Ahmed Ebrahim <eng.ahmed0302@gmail.com>
2025-08-06 22:55:53 +0300

    Initialed project

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -201,27 +201,7 @@
                     </div>
 
 
-                    <div class="menu-item pt-5">
-                        <div class="menu-content">
-                            <span class="menu-heading fw-bold text-uppercase fs-7">الحضور والغياب </span>
-                        </div>
-                    </div>
 
-                    <div class="menu-item">
-                        <a href="{{ route('admin.attendance.index') }}" class="menu-link">
-                            <span class="menu-icon">
-                                <i class="ki-duotone ki-calendar-8 fs-2">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                    <span class="path3"></span>
-                                    <span class="path4"></span>
-                                    <span class="path5"></span>
-                                    <span class="path6"></span>
-                                </i>
-                            </span>
-                            <span class="menu-title"> الحضور والغياب </span>
-                        </a>
-                    </div>
 
 
 
